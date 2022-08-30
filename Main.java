91a9c58f8659b69355d62bb4791bf1d17ce489fd
Calletje234 <calvinvanbeek123@outlook.com>
2022-08-30 00:07:54 -0700

    New commit with better java test code

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main(String arg[]){
-        System.out.println("Hello world");
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] arg){
+        System.out.println("Please type in youre name:");
+        String name = scanner.nextLine();
+        System.out.printf("Youre name is: %s%n", name);
     }
 }
